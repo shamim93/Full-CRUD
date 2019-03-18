a757443c47b56e6b892c635212ca20b542fc7d85
shamim <shamim41.cse@gmail.com>
2019-03-19 04:22:27 +0600

    Initial setup done!

--- RootApp/CRUD/models.py
+++ RootApp/CRUD/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class BookList(models.Model):
+    title = models.CharField(max_length=30)
+    price = models.IntegerField()
+    author = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
